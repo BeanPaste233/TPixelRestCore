93cb89750a4d47fcc37137ea6c7c2f2811bc5163
BeanPaste <1656425255@qq.com>
2021-12-28 16:29:09 +0800

    init

---
+++ Utils.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPixelRestCore
+{
+    public static class Utils
+    {
+
+        public static JObject GetHttp(string url) {
+            using (var client = new HttpClient())
+            {
+                return JObject.Parse(client.GetAsync(url).Result.Content.ReadAsStringAsync().Result);
+            }
+        }
+    }
+}
